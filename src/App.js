3892da9ce2e806a9540eda0f983c9c72d5d7ff54
ibrahim raslan <hemaaraslan92@gmail.com>
2021-07-11 17:39:14 +0200

    creating cert page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Home from "./pages/home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Admin from "./pages/admin";
+import Certe from "./pages/cert";
+import Login from "./pages/login";
+import Validate from "./pages/Validate";
+import { BrowserRouter as Router, Switch, Route,Redirect  } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import './pages/cert.css';
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/certificate-project">
       <Switch>
-        <Route component={Home} path="/" />
+        <Route exact component={Admin} path="/admin" />
+        <Route exact component={Certe}  path="/cert" />
+        <Route exact component={Login} path="/login" />
+        <Route exact component={Validate} path="/validate" />
+        <Route exact  component={Home} path="/" />
+        <Redirect to="/" />
+
       </Switch>
     </Router>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import Box from '@material-ui/core/Box';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,10 +13,13 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 export default function Home() {
   const classes = useStyles();
 
   return (
+    <Box display="flex" justifyContent="center" m={1} p={1}  bgcolor="background.paper">
+    <Box p={1} bgcolor="grey.300">
     <form className={classes.root} noValidate autoComplete="off">
       <div>
         <TextField
@@ -28,10 +32,11 @@ export default function Home() {
         />
       </div>
       <div>
-        <Button variant="contained" color="primary">
-          submit
-        </Button>
+      <button type="button" class="btn btn-outline-primary">Primary</button>
       </div>
     </form>
+    </Box>
+  </Box>
+  
   );
 }
