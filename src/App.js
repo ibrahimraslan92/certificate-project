c56c664bde662740267712ee61557545403cdcb0
Ahmed Hany <dev.ahmedhany@gmail.com>
2021-07-11 22:54:24 +0200

    firebase instead of gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import Home from "./pages/home";
 import Admin from "./pages/admin";
-import Certe from "./pages/cert";
+import Cert from "./pages/cert";
 import Login from "./pages/login";
 import Validate from "./pages/Validate";
-import { BrowserRouter as Router, Switch, Route,Redirect  } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css';
-import './pages/cert.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./pages/cert.css";
 
 export default function App() {
   return (
-    <Router basename="/certificate-project">
+    <Router>
       <Switch>
         <Route exact component={Admin} path="/admin" />
-        <Route exact component={Certe}  path="/cert" />
+        <Route exact component={Cert} path="/cert" />
         <Route exact component={Login} path="/login" />
         <Route exact component={Validate} path="/validate" />
-        <Route exact  component={Home} path="/" />
+        <Route exact component={Home} path="/" />
         <Redirect to="/" />
-
       </Switch>
     </Router>
   );
