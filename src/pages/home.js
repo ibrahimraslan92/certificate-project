427ccb158bd87ee2e859954b787af37fda554d59
ibrahim raslan <hemaaraslan92@gmail.com>
2021-07-12 07:08:28 +0200

    editing home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -19,8 +20,9 @@ export default function Home() {
   const classes = useStyles();
 
   return (
-    <Box display="flex" justifyContent="center" m={1} p={1}  bgcolor="background.paper">
-    <Box p={1} bgcolor="grey.300">
+    <Box display="flex" justifyContent="center" sx={{height:"100vh"}} alignItems="center">
+      <Paper elevation={3} >
+    <Box >
     <form className={classes.root} noValidate autoComplete="off">
       <div>
         <TextField
@@ -32,13 +34,14 @@ export default function Home() {
           variant="outlined"
         />
       </div>
-      <div>
-      <Button variant="contained" color="primary">
+      <Box m={3} display="flex" justifyContent="flex-end">
+      <Button  variant="contained" color="primary">
           submit
       </Button>
-      </div>
+      </Box>
     </form>
     </Box>
+    </Paper>
   </Box>
   
   );
